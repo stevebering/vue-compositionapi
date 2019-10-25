362388224e06cb37ec208e0046a08e22b58c07c6
Steve Bering <stevebering@gmail.com>
2019-10-25 07:45:49 -0700

    Add vue-composition api

---
+++ src/components/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span>Count is: {{ state.count }}, double is: {{ state.double }}</span>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent, computed, reactive } from "@vue/composition-api";
+
+export default createComponent({
+  setup() {
+    const state: any = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    });
+
+    function increment() {
+      state.count++;
+    }
+
+    function decrement() {
+      state.count--;
+    }
+
+    return {
+      state,
+      increment,
+      decrement
+    };
+  }
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Counter />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Counter from "@/components/Counter.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter
   }
 };
 </script>
